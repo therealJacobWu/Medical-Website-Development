2d84721651aa36c17589af33a22fe568769aadf4
Ma Xiwen <moee95@hotmail.com>
2017-11-10 17:19:36 -0600

    update and complete all tests; refactor the ducplicate code in test file.

--- iTrust/test/edu/ncsu/csc/itrust/selenium/PreregisterTest.java
+++ iTrust/test/edu/ncsu/csc/itrust/selenium/PreregisterTest.java
@@ -1,6 +1,13 @@
 package edu.ncsu.csc.itrust.selenium;
 
+import edu.ncsu.csc.itrust.beans.HealthRecord;
+import edu.ncsu.csc.itrust.beans.PatientBean;
+import edu.ncsu.csc.itrust.dao.DAOFactory;
+import edu.ncsu.csc.itrust.dao.mysql.HealthRecordsDAO;
+import edu.ncsu.csc.itrust.dao.mysql.PatientDAO;
 import edu.ncsu.csc.itrust.enums.TransactionType;
+import edu.ncsu.csc.itrust.unit.DBBuilder;
+import edu.ncsu.csc.itrust.unit.testutils.TestDAOFactory;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,17 +21,22 @@ import java.util.concurrent.TimeUnit;
 
 public class PreregisterTest extends iTrustSeleniumTest{
 
+    private DAOFactory factory = TestDAOFactory.getTestInstance();
     public static final String ADDRESS = "http://localhost:8080/iTrust/";
 
     @Override
     protected void setUp() throws Exception {
+        DBBuilder testRebuild = new DBBuilder();
+        testRebuild.main(null);
         super.setUp();
         gen.clearAllTables();
         gen.standardData();
     }
 
-    @Test
-    public void testRegisteAndDuplicateEmail() throws  Exception {
+    /*
+     * Set up a Preregister instance.
+     */
+    protected WebDriver setUp2() {
         WebDriver driver = new HtmlUnitDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(ADDRESS);
@@ -48,9 +60,14 @@ public class PreregisterTest extends iTrustSeleniumTest{
         driver.findElement(By.id("preregistSubmit")).click();
         // Preregister test
         assertTrue(driver.getPageSource().contains("You have pre-registered."));
+        return driver;
+    }
+
+    @Test
+    public void testDuplicateEmail() throws  Exception {
+        WebDriver driver = setUp2();
 
         driver.get(ADDRESS);
-
         // Fill in the prepatient form with the same email address
         driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("testFirstName~");
         driver.findElement(By.xpath("//input[@name='lastName']")).sendKeys("testLastName~");
@@ -101,36 +118,16 @@ public class PreregisterTest extends iTrustSeleniumTest{
 
     @Test
     public void testResetPassword() throws Exception {
-        WebDriver driver = new HtmlUnitDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get(ADDRESS);
+        WebDriver driver = setUp2();
 
-        driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("testFirstName");
-        driver.findElement(By.xpath("//input[@name='lastName']")).sendKeys("testLastName");
-        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("test@gg.com");
-        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("1234");
-        driver.findElement(By.xpath("//input[@name='confirmPassword']")).sendKeys("1234");
-        driver.findElement(By.xpath("//input[@name='streetAddress1']")).sendKeys("testAddr1");
-        driver.findElement(By.xpath("//input[@name='streetAddress2']")).sendKeys("testAddr2");
-        driver.findElement(By.xpath("//input[@name='phone']")).sendKeys("5005005000");
-        driver.findElement(By.xpath("//input[@name='icName']")).sendKeys("testicName");
-        driver.findElement(By.xpath("//input[@name='icAddress1']")).sendKeys("testicAddr1");
-        driver.findElement(By.xpath("//input[@name='icAddress2']")).sendKeys("testicAddr2");
-        driver.findElement(By.xpath("//input[@name='icPhone']")).sendKeys("6006006000");
-        driver.findElement(By.xpath("//input[@name='height']")).sendKeys("123");
-        driver.findElement(By.xpath("//input[@name='weight']")).sendKeys("321");
-        driver.findElement(By.id("smoker1Choice")).click();
-        driver.findElement(By.id("preregistSubmit")).click();
-        assertTrue(driver.getPageSource().contains("You have pre-registered."));
-
-        driver = login("413", "1234");
+        driver = login("411", "1234");
 
         // Wait until redirected to page
         WebDriverWait wait = new WebDriverWait(driver, DEFAULT_TIMEOUT);
         wait.until(ExpectedConditions.titleIs("iTrust - Pre PatientHome"));
 
         // Verify Logging
-        assertLogged(TransactionType.LOGIN_SUCCESS, 413L, 413L, "");
+        assertLogged(TransactionType.LOGIN_SUCCESS, 411L, 411L, "");
 
         //User goes to change password
         List<WebElement> links = driver.findElements(By.tagName("a"));
@@ -156,7 +153,42 @@ public class PreregisterTest extends iTrustSeleniumTest{
     }
 
     @Test
-    public void testOptionalFieldsFills() throws Exception {
+    public void testOptionalFieldsFill() throws Exception {
+        WebDriver driver = new HtmlUnitDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ADDRESS);
 
+        // Fill in the prepatient form with only required fields.
+        driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("testFirstName");
+        driver.findElement(By.xpath("//input[@name='lastName']")).sendKeys("testLastName");
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("test@gg.com");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("1234");
+        driver.findElement(By.xpath("//input[@name='confirmPassword']")).sendKeys("1234");
+        driver.findElement(By.id("preregistSubmit")).click();
+        assertTrue(driver.getPageSource().contains("You have pre-registered."));
+
+        PatientDAO testPatientDao = new PatientDAO(factory);
+        PatientBean testPatientBean = testPatientDao.getPatient(411L);
+        assertEquals(testPatientBean.getMID(), 411L);
+        assertEquals(testPatientBean.getFirstName(), "testFirstName");
+        assertEquals(testPatientBean.getLastName(), "testLastName");
+        assertEquals(testPatientBean.getEmail(), "test@gg.com");
+        assertEquals(testPatientBean.getStreetAddress1(), "");
+        assertEquals(testPatientBean.getStreetAddress2(), "");
+        assertEquals(testPatientBean.getPhone(), "");
+        assertEquals(testPatientBean.getIcName(), "");
+        assertEquals(testPatientBean.getIcAddress1(), "");
+        assertEquals(testPatientBean.getStreetAddress2(), "");
+        assertEquals(testPatientBean.getIcPhone(), "");
+
+        HealthRecordsDAO testHealthRecordDao = new HealthRecordsDAO(factory);
+        try {
+            HealthRecord testHealthRecod = testHealthRecordDao.getAllHealthRecords(411).get(0);
+            assertEquals(testHealthRecod.getHeight(), 0.0);
+            assertEquals(testHealthRecod.getWeight(), 0.0);
+            assertEquals(testHealthRecod.getSmokingStatus(), 9);
+        } catch(IndexOutOfBoundsException e) {
+            System.out.println("PreregisterTest.testOptionalFieldsFill Index out of bound.");
+        }
     }
 }
