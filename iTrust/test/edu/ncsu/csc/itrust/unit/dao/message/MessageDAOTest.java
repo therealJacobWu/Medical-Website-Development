fa3121c84f2fe60dc89ff426bbbdd48f9321bde1
Joey Bloom <15joeybloom@gmail.com>
2017-11-14 00:33:42 -0600

    Simplify MessageDAO test by removing unnecessary MessageBean construction.

--- iTrust/test/edu/ncsu/csc/itrust/unit/dao/message/MessageDAOTest.java
+++ iTrust/test/edu/ncsu/csc/itrust/unit/dao/message/MessageDAOTest.java
@@ -1,6 +1,5 @@
 package edu.ncsu.csc.itrust.unit.dao.message;
 
-import edu.ncsu.csc.itrust.beans.ApptBean;
 import edu.ncsu.csc.itrust.beans.MessageBean;
 import edu.ncsu.csc.itrust.dao.DAOFactory;
 import edu.ncsu.csc.itrust.dao.mysql.MessageDAO;
@@ -9,7 +8,6 @@ import edu.ncsu.csc.itrust.unit.testutils.TestDAOFactory;
 import junit.framework.TestCase;
 
 import java.sql.Timestamp;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
@@ -17,8 +15,8 @@ public class MessageDAOTest extends TestCase {
     private DAOFactory factory = TestDAOFactory.getTestInstance();
     private MessageDAO messageDAO = factory.getMessageDAO();
 
-    private MessageBean m1;
-    private MessageBean m2;
+    private Timestamp date1;
+    private Timestamp date2;
 
     private final long mid = 9000000000L;
 
@@ -32,31 +30,22 @@ public class MessageDAOTest extends TestCase {
         GregorianCalendar cal1 = new GregorianCalendar(2010, 1, 2, 13, 3, 0);
         GregorianCalendar cal2 = new GregorianCalendar(2010, 0, 28, 17, 58, 0);
 
-        m1 = new MessageBean();
-        m1.setMessageId(6);
-        m1.setTo(mid);
-        m1.setSentDate(new Timestamp(cal1.getTimeInMillis()));
-        //m1.setBody("I would like to schedule an appointment before my throat gets any worse. Thanks!");
-
-        m2 = new MessageBean();
-        m2.setMessageId(8);
-        m2.setFrom(mid);
-        m2.setSentDate(new Timestamp(cal2.getTimeInMillis()));
-        //m2.setBody("Hey, I checked on that!");
+        date1 = new Timestamp(cal1.getTimeInMillis());
+        date2 = new Timestamp(cal2.getTimeInMillis());
     }
 
     public void testGetMessagesFor() throws Exception {
         List<MessageBean> results = messageDAO.getMessagesFor(mid);
         assertEquals(results.size(), 14);
 
-        assertEquals(results.get(0).getSentDate(), m1.getSentDate());
+        assertEquals(results.get(0).getSentDate(), date1);
     }
 
     public void testGetMessagesTimeAscending() throws Exception {
         List<MessageBean> results = messageDAO.getMessagesTimeAscending(mid);
         assertEquals(results.size(), 14);
 
-        assertEquals(results.get(13).getSentDate(), m1.getSentDate());
+        assertEquals(results.get(13).getSentDate(), date1);
     }
 
     public void testGetMessagesFrom() throws Exception {
@@ -64,13 +53,13 @@ public class MessageDAOTest extends TestCase {
         assertEquals(results.size(),
                 2);
 
-        assertEquals(results.get(0).getSentDate(), m2.getSentDate());
+        assertEquals(results.get(0).getSentDate(), date2);
     }
 
     public void testGetMessagesFromTimeAscending() throws Exception {
         List<MessageBean> results = messageDAO.getMessagesFromTimeAscending(mid);
         assertEquals(results.size(), 2);
 
-        assertEquals(results.get(1).getSentDate(), m2.getSentDate());
+        assertEquals(results.get(1).getSentDate(), date2);
     }
 }
