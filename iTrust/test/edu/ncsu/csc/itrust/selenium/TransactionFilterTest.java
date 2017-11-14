a4ca0a4b5c5a7de53e0310f92e63ee18d2372e25
Sameet Sapra <sameetandpotatoes@gmail.com>
2017-11-14 13:30:32 -0600

    Add another selenium test for UC39

--- iTrust/test/edu/ncsu/csc/itrust/selenium/TransactionFilterTest.java
+++ iTrust/test/edu/ncsu/csc/itrust/selenium/TransactionFilterTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class TransactionFilterTest extends iTrustSeleniumTest {
 
@@ -54,5 +55,33 @@ public class TransactionFilterTest extends iTrustSeleniumTest {
         assertNotNull(driver.findElement(By.id("container1")));
         assertNotNull(driver.findElement(By.id("container2")));
         assertNotNull(driver.findElement(By.id("container3")));
+    }
+
+    @Test
+    public void testTransactionFilteringValid1() throws Exception {
+        driver = login("9000000001", "pw"); //log in as Admin
+
+        assertEquals("iTrust - Admin Home", driver.getTitle());
+
+        //go to the page
+        element = driver.findElement(By.linkText("Admin Transaction Log"));
+        element.click();
+
+        assertEquals("FOR TESTING PURPOSES ONLY", driver.getTitle());
+
+        Select loggedInUserField = new Select(driver.findElement(By.name("loggedInUserField")));
+        loggedInUserField.selectByValue("hcp");
+
+        Select secondaryUserField = new Select(driver.findElement(By.name("secondaryUserField")));
+        secondaryUserField.selectByValue("patient");
+
+        driver.findElement(By.name("startDate")).sendKeys("01/01/2007");
+        driver.findElement(By.name("endDate")).sendKeys("12/31/2008");
+
+        Select transactionType = new Select(driver.findElement(By.name("transactionType")));
+        transactionType.selectByValue("-1");
+
+        driver.findElement(By.cssSelector("input[value='View']")).submit();
+        assertEquals(34, driver.findElements(By.className("transaction_row")).size());
     }
 }
