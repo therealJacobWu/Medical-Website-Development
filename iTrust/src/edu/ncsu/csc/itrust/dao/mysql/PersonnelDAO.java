77c81fcb7e6d62258dabbe4995d29111d7a6dd4a
ChaseDuncan <cddunca2@illinois.edu>
2017-11-16 12:38:21 -0600

    Edit message filter in place for personnel.

--- iTrust/src/edu/ncsu/csc/itrust/dao/mysql/PersonnelDAO.java
+++ iTrust/src/edu/ncsu/csc/itrust/dao/mysql/PersonnelDAO.java
@@ -541,7 +541,7 @@ public class PersonnelDAO {
 			DBUtil.closeConnection(conn, ps);
 		}
 	}
-	
+
 	/**
 	 * Returns all of the personnel who have a specialty of nutritionist
 	 */
@@ -589,4 +589,48 @@ public class PersonnelDAO {
 			DBUtil.closeConnection(conn, ps);
 		}
 	}
+
+    private void validateMessageFilterColumn() throws DBException{
+     	Connection conn = null;
+		PreparedStatement ps = null;
+
+		try {
+			conn = factory.getConnection();
+			ps = conn.prepareStatement("SHOW COLUMNS FROM personnel LIKE 'messagefilter' ");
+			ResultSet rs = ps.executeQuery();
+            if (!rs.first() ) {
+                ps = conn.prepareStatement("ALTER TABLE personnel ADD COLUMN messagefilter varchar(1024)");
+                rs = ps.executeQuery();
+            }
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+		} finally {
+			DBUtil.closeConnection(conn, ps);
+		}
+    }
+
+    public void editMessageFilter(long pid, String filter) throws DBException{
+        validateMessageFilterColumn();
+        Connection conn = null;
+		PreparedStatement ps = null;
+
+		try {
+			conn = factory.getConnection();
+            //ps = conn.prepareStatement("DELETE FROM messagefilters WHERE MID=?");
+            ps = conn.prepareStatement("UPDATE personnel SET messagefilter=? WHERE MID=?");
+            ps.setString(1, filter);
+            ps.setLong(2, pid);
+			ps.executeUpdate();
+			//ps = conn.prepareStatement("INSERT INTO messagefilters (MID,filter) VALUES (?,?)");
+            //ps.setLong(1, pid);
+            //ps.setString(2, filter);
+			//rs = ps.executeQuery();
+			ps.close();
+		} catch (SQLException e) {
+			throw new DBException(e);
+		} finally {
+			DBUtil.closeConnection(conn, ps);
+		}
+    }
 }
