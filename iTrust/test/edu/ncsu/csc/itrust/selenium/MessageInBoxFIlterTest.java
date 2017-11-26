9503a73cc555af634e6abe116bfe410f2b63fa56
Chenhao Wu <chenhaowu@ChenhaotekiMacBook-Pro.local>
2017-11-26 14:48:50 -0600

    rename MessageInBoxFilterTest

--- iTrust/test/edu/ncsu/csc/itrust/selenium/MessageInBoxFIlterTest.java
+++ iTrust/test/edu/ncsu/csc/itrust/selenium/MessageInBoxFIlterTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class MessageInBoxFIlterTest extends iTrustSeleniumTest{
+public class MessageInBoxFilterTest extends iTrustSeleniumTest{
 
     WebDriver driver;
     WebElement element;
